1aebe289bee2c3572483fd0f2f22439d12b19140
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-04-30 10:24:58 -0700

    refactor configs

--- pytorch/launch.py
+++ pytorch/launch.py
@@ -2,20 +2,21 @@
 """Launch training on AWS with 8 GPUs."""
 
 import argparse
+import re
+
 import ncluster
 from ncluster import aws_util
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', type=str, default='txl',
                     help="name of the current run, used for machine naming and tensorboard visualization")
-parser.add_argument('--machines', type=int, default=1,
+parser.add_argument('--machines', type=int, default=0,
                     help="how many machines to use")
-parser.add_argument('--instance_type', type=str, default="p3.16xlarge",
+parser.add_argument('--instance_type', type=str, default='',
                     help="how many machines to use")
+
 parser.add_argument('--nospot', action='store_true',
                     help='Use more expensive on-demand instance')
-parser.add_argument('--ncluster_backend', type=str, default='aws',
-                    help='Use spot instance')
 
 parser.add_argument('--skip_setup', action='store_true',
                     help='Make startup slightly faster by skiping various initialization tasks, like '
@@ -27,8 +28,51 @@ parser.add_argument('--image_name', type=str, default='reference03',
 parser.add_argument('--conda_env', type=str, default='pytorch_p36',
                     help='which conda env to use')
 
+parser.add_argument('--checkpoint-each-epoch', type=int, default=0,
+                    help='whether to save checkpoint at each epoch')
+
 # network settings
 parser.add_argument('--num_rings', type=int, default=16)
+
+
+# learning settings
+parser.add_argument('--config', type=str, default='',
+                    help='which training config to use')
+
+
+# 'base_lr': learning rate for BASE_LR_BATCHSIZE, actual learning rate will apply linear scaling according to
+#   the actual batch size used
+# local_batch: per-GPU batch size
+BASE_LR_BATCHSIZE = 32
+
+one_gpu = {
+    # 24x smaller batch than ben-big-lr.09, use 5x more agressive learning rate
+    'base_lr': 0.000125*5/3*5,   
+    'local_batch': 32,
+    'instance_type': 'p3.2xlarge',
+    'machines': 1
+}
+
+one_machine = {
+    'base_lr': 0.000125*5/3,  # ben-big-lr.09
+    'instance_type': 'p3dn.24xlarge',
+    'local_batch': 96,
+    'machines': 1,
+}
+
+two_machines = {23
+    'base_lr': 0.000125*5/3,    # yaro-two.07
+    'instance_type': 'p3dn.24xlarge',
+    'local_batch': 96,
+    'machines': 2,
+}
+
+eight_machines = {
+    'base_lr': 0.000125,
+    'instance_type': 'p3dn.24xlarge',
+    'local_batch': 96,
+    'machines': 8,
+}
 
 
 def get_nccl_params(_num_tasks, _num_gpus):
@@ -36,24 +80,37 @@ def get_nccl_params(_num_tasks, _num_gpus):
 
     if args.machines > 1:
         params += f'NCCL_MIN_NRINGS={args.num_rings} NCCL_MAX_NRINGS={args.num_rings} '
-    assert aws_util.instance_supports_100gbps_network(args.instance_type), \
-        "set this to proper socket name for non p3dn instances"
-    params += f'NCCL_SOCKET_IFNAME=ens5 '
     return params
 
 
-# reference schedule for 1 GPU, batch size 32 run
-lr_schedules = {1: {'base_lr': 0.000125*5/3},  # ben-big-lr.09
-                2: {'base_lr': 0.000125*5/3},    # yaro-two.07
-                8: {'base_lr': 0.000125}}
-
-
 def main(_unused_args_using_global_args_instead):
-    ncluster.set_backend(args.ncluster_backend)
+    ncluster.set_backend('aws')
     ncluster.set_logdir_root('/ncluster/runs.new')  # TODO(y): /ncluster/runs
+
+    if args.config:
+        assert not args.instance_type, "specify instance_type as part of config"
+        assert not args.machines, "specify number of machines as part of config"
+        assert re.match('\\w+', args.config)
+        assert args.config in globals()
+        schedule = eval(args.config)
+        args.machines = schedule['machines']
+
+    else:  # legacy way of setting config
+        assert args.instance_type
+        assert args.machines
+        schedule = {
+            'base_lr': 0.000125*5/3,  # ben-big-lr.09
+            'local_batch': 96,
+        }
+        schedule['instance_type'] = args.instance_type
+        schedule['machines'] = args.machines
+        
+
+    args.instance_type = schedule['instance_type']
+    num_gpus = ncluster.aws_backend.INSTANCE_INFO[args.instance_type]['gpus']
     job = ncluster.make_job(name=args.name,
                             run_name=f"{args.name}",
-                            num_tasks=args.machines,
+                            num_tasks=schedule['machines'],
                             image_name=args.image_name,
                             instance_type=args.instance_type,
                             spot=not args.nospot,
@@ -74,26 +131,23 @@ def main(_unused_args_using_global_args_instead):
         '--distributed',
     ]
 
-    num_gpus = ncluster.aws_backend.INSTANCE_INFO[args.instance_type]['gpus']
-    # gpu_mem_gb = ncluster.aws_backend.INSTANCE_INFO[args.instance_type]['gpu_mem_gb']
-
-    local_batch = 32
-    #    base_lr = 0.00025  # reference lr for 1 worker, batch size 64
-    base_lr = lr_schedules[args.machines]['base_lr']
+    local_batch = schedule['local_batch']
+    base_lr = schedule['base_lr']
 
     num_workers = num_gpus * args.machines
     global_batch = local_batch * num_workers
     print("using global batch ", global_batch)  # 512=8*32*2*1
 
-    if '24x' in args.instance_type:  # special config for increased batch size
-        local_batch = 96  # nonlinear bs scaling
-        _global_batch = local_batch * num_workers
-        base_lr = base_lr * 3  # linear scaling for 32 -> 96 batch size
-        
-    lr = base_lr * num_workers
+    #    if '24x' in args.instance_type:  # special config for increased batch size
+    #        local_batch = 96  # nonlinear bs scaling
+    #        _global_batch = local_batch * num_workers
+    #        base_lr = base_lr * 3  # linear scaling for 32 -> 96 batch size
+
+    # linear LR scaling (https://arxiv.org/abs/1706.02677)
+    lr = base_lr * (global_batch / BASE_LR_BATCHSIZE)
 
     # todo(y): consistency with - and _ in args
-    # Based on run_wt103_base.sh
+    # Based on run_wt103_base.sh, tweaked to be multiples of 8
     # todo(y): data should be baked into image rather than EFS
     training_params = [
         '--seed', 1111,
@@ -103,10 +157,10 @@ def main(_unused_args_using_global_args_instead):
         '--adaptive',
         '--log-interval', 100,
         '--n_layer', 16,
-        '--d_model', 410,
-        '--n_head', 10,
-        '--d_head', 41,
-        '--d_inner', 2100,
+        '--d_model', 512,
+        '--n_head', 8,
+        '--d_head', 48,
+        '--d_inner', 2048,
         '--dropout', 0.1,
         '--dropatt', 0.0,
         '--optim', 'lamb',
@@ -122,7 +176,14 @@ def main(_unused_args_using_global_args_instead):
         # '--scheduler', 'finder', # Use max_tokens 2e7 and log-interval 10
     ]
 
+    # todo(y) rename to params, change to extend
     training_params = default_params + training_params
+
+    # pass through command-line launcher arguments to the worker
+    user_params = ['--checkpoint-each-epoch', args.checkpoint_each_epoch]
+
+    training_params.extend(user_params)
+    
     training_params = ' '.join(str(p) for p in training_params)
     nccl_params = get_nccl_params(args.machines, num_gpus)
 
