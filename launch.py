58d35bc81ceba2c13be51d219969a3c490243581
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-05-02 11:30:42 -0700

    clean-up

--- launch.py
+++ launch.py
@@ -1,41 +1,28 @@
 #!/usr/bin/env python
 """Launch training on AWS with 8 GPUs."""
 
-import argparse
-from attrdict import AttrDict, AttrDefault
-import re
-import util
 
+from attrdict import AttrDict
+import argparse
 import ncluster
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', type=str, default='txl',
-                    help="name of the current run, used for machine naming and rundir name")
+                    help="name of the current run, used for machine naming and logging directory")
 parser.add_argument('--config', type=str, default='',
                     help='which training config to use')
 parser.add_argument('--nospot', action='store_true',
                     help='Use more expensive on-demand instance')
-
 parser.add_argument('--skip_setup', action='store_true',
-                    help='Make startup slightly faster by skiping various initialization tasks, like '
+                    help='Make startup faster by skiping various initialization tasks, like '
                          'tmux/efs setup. Only use on reruns.')
 
-# network settings
+# Flags that affect all configs
 parser.add_argument('--num_rings', type=int, default=16)
-
-# config flags for trying configurations outside of existing configs
-parser.add_argument('--machines', type=int, default=0,
-                    help="how many machines to use")
-parser.add_argument('--instance_type', type=str, default='',
-                    help="how many machines to use")
-parser.add_argument('--checkpoint_each_epoch', type=int, default=0,
-                    help='whether to save checkpoint at each epoch')
 parser.add_argument('--image_name', type=str, default='reference03',
                     help="use custom AMI ")
 parser.add_argument('--conda_env', type=str, default='pytorch_p36',
                     help='use custom conda env')
-parser.add_argument('--checkpoint', type=str, default='',
-                    help='restore from this checkpoint')
 args = parser.parse_args()
 
 # Config notes:
@@ -60,7 +47,6 @@ one_machine_fp16 = {
     'instance_type': 'p3dn.24xlarge',
     'local_batch_size': 96,
     'machines': 1,
-    'extra_worker_params': ['--fp16', '--dynamic_loss_scale']
 }
 
 
@@ -70,7 +56,6 @@ two_machines_fp16 = {
     'instance_type': 'p3dn.24xlarge',
     'local_batch_size': 96,
     'machines': 2,
-    'extra_worker_params': ['--fp16', '--dynamic_loss_scale']
 }
 
 # yaro-four
@@ -79,33 +64,21 @@ four_machines = {
     'instance_type': 'p3dn.24xlarge',
     'local_batch_size': 96,
     'machines': 4,
-    'extra_worker_params': ['--fp16', '--dynamic_loss_scale']
 }
 
+# logs: yaro-eight.03
 eight_machines = {
     'base_lr': 0.000125/2,  # remove ben's 5/3 tweak, and additional penalty of 2x
     'instance_type': 'p3dn.24xlarge',
     'local_batch_size': 96,
     'machines': 8,
     'checkpoint': '/ncluster/runs.new/yaro-one.08/model-1.pt',
-    'extra_worker_params': ['--fp16', '--dynamic_loss_scale',
-                            '--warmup_tokens', 50e6]
 }
-
-
-def _get_nccl_params():
-    params = f'NCCL_DEBUG=VERSION '
-
-    params += f'NCCL_MIN_NRINGS={args.num_rings} ' \
-        f'NCCL_MAX_NRINGS={args.num_rings} '
-    return params
 
 
 if __name__ == '__main__':
     assert args.config in globals(), f"unknown config {args.config}"
-    config = eval(args.config)
-
-    config = AttrDefault(str, config)     # easier access to dictionary entries
+    config = AttrDict(eval(args.config))  # easier access to dictionary entries
 
     config.image_name = args.image_name
     config.conda_env = args.conda_env
@@ -123,16 +96,17 @@ if __name__ == '__main__':
             f'source activate {config.conda_env} && ' +
             f'pip install -r requirements.txt')
 
-    local_batch_size = config.local_batch_size
-    base_lr = config.base_lr
-    num_workers = num_gpus_per_machine * config.machines
-    global_batch_size = local_batch_size * num_workers
+    instance_info = ncluster.aws_backend.INSTANCE_INFO[config.instance_type]
+    num_gpus_per_machine = instance_info['gpus']
+
+    total_gpus = num_gpus_per_machine * config.machines
+    global_batch_size = config.local_batch_size * total_gpus
 
     # linear LR scaling (https://arxiv.org/abs/1706.02677)
-    lr = base_lr * (global_batch_size / BASE_LR_BATCHSIZE)
+    lr = config.base_lr * (global_batch_size / BASE_LR_BATCHSIZE)
 
     # worker parameters with training setup
-    training_params = [
+    worker_params = [
         '--seed', 1111,
         '--data', '/ncluster/data/transformer-xl-data/wikitext-103',
         '--dataset', 'wt103',
@@ -152,41 +126,23 @@ if __name__ == '__main__':
         '--tgt_len', 128,
         '--mem_len', 128,
         '--eval_tgt_len', 128,
-        '--batch_size', local_batch_size,  # per-gpu batch size
+        '--batch_size', config.local_batch_size,  # per-gpu batch size
         '--eval_interval', 4000,
+        '--fp16',
+        '--dynamic_loss_scale',
+        '--distributed',
+        '--logdir', job.logdir,
     ]
 
-    worker_params = ['--logdir', job.logdir,
-                     '--distributed']
-    worker_params.extend(training_params)
-
-    user_params = []
-    # pass through some user-provided settings that were arguments to the launcher script
-    if args.checkpoint_each_epoch:
-        user_params.extend(['--checkpoint_each_epoch', args.checkpoint_each_epoch])
-
-    if args.checkpoint or config.checkpoint:
-        user_params.extend(['--checkpoint',
-                            util.one_of([args.checkpoint, config.checkpoint])])
-
-    if config.warmup_tokens:
-        user_params.extend(['--warmup_tokens', config.warmup_tokens])
-        
-
-    worker_params.extend(user_params)
-
-    if 'extra_worker_params' in config:
-        worker_params.extend(config.extra_worker_params)
-
-    worker_params = ' '.join(str(p) for p in worker_params)
-    nccl_params = _get_nccl_params()
+    nccl_params = f'NCCL_DEBUG=VERSION NCCL_MIN_NRINGS={args.num_rings} '
 
     for i, task in enumerate(job.tasks):
         dist_params = \
             f'--nproc_per_node={num_gpus_per_machine} ' \
             f'--nnodes={config.machines} --node_rank={i} ' \
             f'--master_addr={job.tasks[0].ip} --master_port={6016}'
-        cmd = f'{nccl_params} python -m torch.distributed.launch {dist_params} train.py {worker_params}'
+        cmd = f'{nccl_params} python -m torch.distributed.launch {dist_params} ' \
+              f'train.py {worker_params}'
         task.run(f'echo {cmd} > {job.logdir}/task-{i}.cmd')  # save command-line
         task.run(cmd, non_blocking=True)
 
